a5bec3c3f51a590e18b6b2567ac2752df2ed1940
RiverTwilight <wrj2014@126.com>
2020-07-04 19:34:08 +0800

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export async function getStaticProps() {
                 defaultTitle: slug,
                 frontmatter: document.data,
                 markdownBody: document.content,
-                slug,
+                slug: encodeURI(slug),
             }
         })
         return data
