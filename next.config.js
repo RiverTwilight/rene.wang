4a5d6f1fb5e937b2c65f97ee2f89b810e4a355c1
RiverTwilight <wrj2014@126.com>
2021-04-11 08:48:28 +0800

    fix: remove unnecessary sass plugin fix: downgrade next to 10.0.3

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const withSass = require("@zeit/next-sass");
-module.exports = withSass({
+module.exports = {
 	images: {
 		sizes: [320, 480, 820, 1200, 1600],
 		domains: ["i.loli.net", "bgr.com"],
@@ -22,4 +21,4 @@ module.exports = withSass({
 		locales: ["en-US", "zh-CN"],
 		defaultLocale: "zh-CN",
 	},
-});
+};
