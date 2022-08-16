0f970ed086486f3c88a62bc58daf4a68c4f946de
RiverTwilight <wrj2014@126.com>
2022-08-16 14:48:13 +0800

    fix: image display abnormaly

--- components/LazyloadImage.tsx
+++ components/LazyloadImage.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import Image from "next/image";
 
-// TODO 图片N响应式设计
-
 export default ({ src, alt }: { src: string; alt?: string }) => {
 	return (
 		<>
-			<Image width="100" height="300" alt={alt} src={src} />
-			{alt && <div className="typo-img-caption">{alt}</div>}
+			<div
+				style={{
+					position: "relative",
+					width: "100%",
+					paddingBottom: "20%",
+				}}
+			>
+				<Image layout="fill" objectFit="contain" alt={alt} src={src} />
+
+				{alt && <div className="typo-img-caption">{alt}</div>}
+			</div>
 		</>
 	);
 };
