da6d22e158d016ddb2e66b12c656056c9741528e
RiverTwilight <wrj2014@126.com>
2022-11-18 09:31:18 +0800

    edit post

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 import siteConfig from "../site.config.js";
 import { Container } from "@kindle-ui/core";
 import "./App.css";
